4d8ad65af4c1ad4e040bb88905bca827552dfed6
mohamaad01 <mohamaad0y@gmail.com>
2025-06-23 16:59:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let userId = "demo_user"; // fallback for local testing
 
 if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initDataUnsafe && window.Telegram.WebApp.initDataUnsafe.user && window.Telegram.WebApp.initDataUnsafe.user.id) {
     userId = window.Telegram.WebApp.initDataUnsafe.user.id;
+}
 
 // === Utility Functions ===
 const showAlert = (message) => alert(message);
